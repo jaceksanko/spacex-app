d32356be956551f664951abbe896d506ec6a738c
jaceksanko <jacek.sanko@gmail.com>
2022-02-25 17:45:09 +0100

    Add pagination

--- src/ApiProvider.tsx
+++ src/ApiProvider.tsx
@@ -1,13 +1,26 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const client = new ApolloClient({
-  uri: 'https://api.spacex.land/graphql/',
-  cache: new InMemoryCache()
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          launchesPast: {
+            keyArgs: false,
+            merge(existing = [], incoming: any) {
+              return [...existing, ...incoming];
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default function ApiProvider({ children }: Props) {
--- src/FlightListPage.tsx
+++ src/FlightListPage.tsx
@@ -2,11 +2,16 @@ import { gql, useQuery } from "@apollo/client";
 
 import FlightList from "./FlightList";
 import FlightListFilters from "./FlightListFilters";
-import {useState} from "react";
+import { useCallback, useState } from "react";
 
 const FLIGHT_LIST_QUERY = gql`
-  query GetPastLaunches($missionName: String) {
-    launchesPast(find: {mission_name: $missionName}, limit: 10, sort: "launch_date_local", order: "asc") {
+  query GetPastLaunches($missionName: String, $offset: Int) {
+    launchesPast(
+      find: { mission_name: $missionName }
+      limit: 10
+      sort: "launch_date_local"
+      offset: $offset
+    ) {
       id
       launch_date_local
       launch_site {
@@ -26,16 +31,35 @@ const FLIGHT_LIST_QUERY = gql`
 `;
 
 export default function FlightListPage() {
-  const [missionName, setMissionName] = useState('');
-  const { loading, data } = useQuery(FLIGHT_LIST_QUERY, {
-    variables: { missionName },
+  const [missionName, setMissionName] = useState("");
+  const [prevData, setPrevData] = useState({ launchesPast: [] });
+  const { loading, data, fetchMore, client } = useQuery(FLIGHT_LIST_QUERY, {
+    variables: { missionName, offset: 0 },
+    fetchPolicy: "network-only",
   });
+
+  const handleClickButton = useCallback(async () => {
+    await fetchMore({
+      variables: { missionName, offset: data.launchesPast.length },
+    });
+    setPrevData(data);
+  }, [fetchMore, data]);
 
   return (
     <>
       <h2>Launch list</h2>
-      <FlightListFilters updateMissionName={(value: string) => setMissionName(value)}/>
-      {!loading && <FlightList launches={data.launchesPast}/>}
+      <FlightListFilters
+        updateMissionName={async (value: string) => {
+          await client.clearStore();
+          setPrevData({ launchesPast: [] });
+          setMissionName(value);
+        }}
+      />
+      {!loading && <FlightList launches={data.launchesPast} />}
+      {(prevData.launchesPast.length !== data?.launchesPast.length ||
+        data.length === 10) && (
+        <button onClick={handleClickButton}>Więcej</button>
+      )}
     </>
   );
 }
